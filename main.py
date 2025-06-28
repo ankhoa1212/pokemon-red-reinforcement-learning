cf83795c09853090fe67f92880ec26cda462c954
Austin <ankhoa1212@gmail.com>
2025-06-28 03:09:50 -0700

    add testing of random inputs

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from pokemon_red_env import PokemonRedEnv
+import gymnasium as gym
 from gymnasium.utils.env_checker import check_env
 
 IMAGE_DIRECTORY = 'images/'  # directory to images
 SCREENSHOT_FILENAME = 'screenshot.png'
 MASTER_MAP_FILENAME = 'master_map.png'  # master map of all areas
 
-map = {}  # map of area names to coordinate and corresponding image files
-pos = (0, 0)
-
 def create_env(env_settings):
     env = PokemonRedEnv(settings=env_settings)
-    try:
-        check_env(env)
-    except Exception as e:
-        print(f"Environment check failed: {e}")
+    # try:
+    #     check_env(env, warn=True, skip_render_check=True)
+    # except Exception as e:
+    #     print(f"Environment check failed: {e}")
     return env
 
 # TODO consider multiple environments for parallel training
@@ -22,9 +20,15 @@ if __name__ == "__main__":
     env_settings = {
         "game_path": "pokemon_red.gb",
         "debug": False,
-        "frames_per_action": 24,
+        "frame_rate": 24,
         "map": IMAGE_DIRECTORY + MASTER_MAP_FILENAME,
-        "output_shape": (160, 144, 1),
-        "max_steps": 1000000
+        "output_shape": (144, 160),
+        "max_steps": 1000,
+        "image_directory": IMAGE_DIRECTORY,
+        "view": "SDL2"
     }
     environment = create_env(env_settings)
+    while True:
+        action = environment.action_space.sample()  # sample a random action
+        obs, rewards, terminated, truncated, info = environment.step(action)
+        environment.render()
